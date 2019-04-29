991ee6c4f40cf8e07c557ae595972f1e403d81f7
laoolu <oyewosamuel2011@gmail.com>
2019-04-29 13:41:54 +0100

    This is my first commit

---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MusicHub.Models;
+
+namespace MusicHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
